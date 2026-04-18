7cd77f176e496cd04f22bff17cc42fe809c20901
Thiago-Vespasiano <thiago.vespasiano@unifesp.br>
2026-04-17 23:15:22 -0300

    Create 1000.c

---
+++ 1000.c
@@ -0,0 +1,16 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Vespasiano Santos
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 17/04/2026
+Objetivo    : Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha
+Aprendizado : Utilização da função "printf"
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+
+int main (void) {
+    printf ("Hello World!");
+    return 0;
+}
