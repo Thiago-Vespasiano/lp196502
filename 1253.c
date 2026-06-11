db59bd620c4e1128c75ec2206ee55ca0bdd964d2
Thiago-Vespasiano <thiago.vespasiano@unifesp.br>
2026-06-11 14:16:07 -0300

    Create 1253.c

---
+++ 1253.c
@@ -0,0 +1,32 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Vespasiano
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Data        : 11/06/2026
+Objetivo    : cifra de cesar
+Aprendizado : strings
+Pergunta de segunda ordem: 
+-------------------------------------------------------------------------- *
+  #include <string.h>
+#include <stdio.h>
+
+int main(){
+    char alfabeto[51];
+    int N, deslocamento;
+
+    scanf("%d\n", &N);
+
+    for(int i = 0; i < N; ++i){
+        scanf("%[^\n]\n", &alfabeto);
+        scanf("%d\n", &deslocamento);
+
+        for(int j = 0; j < strlen(alfabeto); ++j){
+            alfabeto[j] = ((alfabeto[j] - 'A' - deslocamento + 26) % 26) + 'A';
+        }
+
+        printf("%s\n", alfabeto);
+    }
+
+    return 0;
+}
