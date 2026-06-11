aadc6a16aab159989702d429e8140fbd6225f40a
Thiago-Vespasiano <thiago.vespasiano@unifesp.br>
2026-06-11 14:26:20 -0300

    Update 1253.c

--- 1253.c
+++ 1253.c
@@ -2,7 +2,7 @@
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Thiago Vespasiano
 Linguagem   : C
-Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Problema    : https://judge.beecrowd.com/pt/problems/view/1253
 Data        : 11/06/2026
 Objetivo    : cifra de cesar
 Aprendizado : strings
