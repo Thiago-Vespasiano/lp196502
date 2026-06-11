a735687da331a5ef99063fd7631be5c005c98591
Thiago-Vespasiano <thiago.vespasiano@unifesp.br>
2026-06-11 14:32:34 -0300

    Create 1024.c

---
+++ 1024.c
@@ -0,0 +1,10 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Vespasiano
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1024
+Data        : 11/06/2026
+Objetivo    : Criptografia
+Aprendizado : strings
+Pergunta de segunda ordem: 
+-------------------------------------------------------------------------- *
