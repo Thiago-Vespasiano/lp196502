f229aa06c5f3891463bf998e72b7a84d99b08ca1
Thiago-Vespasiano <thiago.vespasiano@unifesp.br>
2026-06-16 16:33:11 -0300

    Create 1015.c

---
+++ 1015.c
@@ -0,0 +1,24 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Vespasiano
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1015
+Data        : 16/06/2026
+Objetivo    : Distância Entre Dois Pontos
+Aprendizado : math.h
+Pergunta de segunda ordem: 
+-------------------------------------------------------------------------- *
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double x1, y1, x2, y2, distancia;
+
+    scanf("%lf %lf\n%lf %lf", &x1, &y1, &x2, &y2);
+
+    distancia = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+
+    printf("%.4lf\n", distancia);
+
+    return 0;
+}
