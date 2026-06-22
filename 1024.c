43d16d9669a821e3ce4dd00a086a032ea937bc50
Thiago-Vespasiano <thiago.vespasiano@unifesp.br>
2026-06-22 14:15:11 -0300

    Update 1024.c

--- 1024.c
+++ 1024.c
@@ -8,3 +8,51 @@ Objetivo    : Criptografia
 Aprendizado : strings
 Pergunta de segunda ordem: 
 -------------------------------------------------------------------------- *
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+    getchar();
+
+    for(int k = 0; k < n; k++)
+    {
+        char frase[1001];
+
+        fgets(frase, 1001, stdin);
+
+        frase[strcspn(frase, "\n")] = '\0';
+
+        for(int i = 0; i < strlen(frase); i++)
+        {
+            if((frase[i] >= 'A' && frase[i] <= 'Z') ||
+               (frase[i] >= 'a' && frase[i] <= 'z'))
+            {
+                frase[i] += 3;
+            }
+        }
+
+        int tam = strlen(frase);
+
+        for(int i = 0, j = tam - 1; i < j; i++, j--)
+        {
+            char aux = frase[i];
+            frase[i] = frase[j];
+            frase[j] = aux;
+        }
+
+        int meio = tam / 2;
+
+        for(int i = meio; i < tam; i++)
+        {
+            frase[i]--;
+        }
+
+        printf("%s\n", frase);
+    }
+
+    return 0;
+}
