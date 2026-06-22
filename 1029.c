0c0453b2f08c95ef4897710eec36d7fe32547cf6
Thiago-Vespasiano <thiago.vespasiano@unifesp.br>
2026-06-22 16:27:13 -0300

    Create 1029.c

---
+++ 1029.c
@@ -0,0 +1,45 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Vespasiano Santos
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 17/04/2026
+Objetivo    : Fibonacci, Quantas Chamadas?
+Aprendizado : recursao
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int calls;
+
+int fib(int n)
+{
+    calls++;
+
+    if(n == 0)
+        return 0;
+
+    if(n == 1)
+        return 1;
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int t, n;
+
+    scanf("%d", &t);
+
+    for(int i = 0; i < t; i++)
+    {
+        scanf("%d", &n);
+
+        calls = 0;
+
+        int result = fib(n);
+
+        printf("fib(%d) = %d calls = %d\n", n, calls - 1, result);
+    }
+
+    return 0;
+}
