94202c698a2b0a6c6d0c7968bc27282ee5c42472
Thiago-Vespasiano <thiago.vespasiano@unifesp.br>
2026-06-22 14:10:52 -0300

    Update 1241.c

--- 1241.c
+++ 1241.c
@@ -8,24 +8,50 @@ Objetivo    : Encaixa ou Não II
 Aprendizado : strings
 Pergunta de segunda ordem: 
 -------------------------------------------------------------------------- *
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-    int N;
-    char A[1001], B[1001];
+int main()
+{
+    int n;
 
-    scanf("%d\n", &N);
+    scanf("%d", &n);
 
-    for(int i = 0; i < N; ++i){
-        scanf("%s %s\n", &A, &B);
+    for(int k = 0; k < n; k++)
+    {
+        char a[1001];
+        char b[1001];
 
-        if(strlen(B) > strlen(A))   printf("nao encaixa\n");
-        else{
-            strcpy(A, A + (strlen(A) - strlen(B)));
-            if(!strcmp(A, B))       printf("encaixa\n");
-            else                    printf("nao encaixa\n");
+        scanf("%s", a);
+        scanf("%s", b);
+
+        int tamA = strlen(a);
+        int tamB = strlen(b);
+
+        int encaixa = 1;
+
+        if(tamB > tamA)
+        {
+            encaixa = 0;
         }
+        else
+        {
+            int inicio = tamA - tamB;
+
+            for(int i = 0; i < tamB; i++)
+            {
+                if(a[inicio + i] != b[i])
+                {
+                    encaixa = 0;
+                    break;
+                }
+            }
+        }
+
+        if(encaixa)
+            printf("encaixa\n");
+        else
+            printf("nao encaixa\n");
     }
 
     return 0;
